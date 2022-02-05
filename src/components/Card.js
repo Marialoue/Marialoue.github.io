ebcea33d1d48965762dc1cd3fd655d823864b761
Maria Karlsson <40427644+Marialoue@users.noreply.github.com>
2022-02-05 19:45:38 +0100

    Add card and grid container component

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+// import Chip from "./Chip";
+
+export default styled.article`
+border: 1px solid ${({theme}) => theme.bgBtn};
+border-radius: var(--border-radius);
+padding: 1rem;
+`;
+
+
+ 
---
+++ src/components/GridContainer.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export default styled.section`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 1rem;
+    padding: 1rem;
+    margin: auto;
+`;
