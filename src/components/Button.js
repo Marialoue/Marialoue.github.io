d933bf101ed52ec4628e44e6d8072aa96ab8efe6
Maria Karlsson <40427644+Marialoue@users.noreply.github.com>
2022-02-09 20:04:27 +0100

    Delete btn link component as it's almost the same as btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,22 @@
 import styled from "styled-components";
 
 export default styled.button`
-  background: transparent;
-  border: none;
+  display: inline-flex;
+  place-content: center;
   padding: 0.5rem;
-  color: ${({ theme }) => theme.colorBtn};
+  margin: 0.2rem;
+  background: transparent;
   cursor: pointer;
+  border-radius: 50%;
+  border: 2px solid ${({ theme }) => theme.bgBtn50};
+  color: ${({ theme }) => theme.bgBtn50};
+  transition: ${({ theme }) => theme.transition};
+
+  &:hover {
+    background: ${({ theme }) => theme.bg40};
+    border: 2px solid ${({ theme }) => theme.bgBtn};
+    svg {
+      stroke: ${({ theme }) => theme.bgBtn};
+    }
+  }
 `;
